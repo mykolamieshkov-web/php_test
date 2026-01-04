6fe37f74be46821f40deb6da7265652e70f5932c
mykolamieshkov-web <mykolamieshkov@gmail.com>
2026-01-04 18:19:59 +0300

    build with latest php version

--- api/test_vercel_php.php
+++ api/test_vercel_php.php
@@ -1,16 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
-
-$input = json_decode(file_get_contents('php://input'), true);
-$clientMessage = $input['message'] ?? 'No message';
+$clientMessage = $_GET['message'] ?? 'No message';
 
 $response = [
     'success' => true,
